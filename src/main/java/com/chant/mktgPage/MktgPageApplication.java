a7e758188a0a66124308159131560432b1af1d99
Ryan Jenkins <ryanjenkins@Ryans-Air.att.net>
2018-02-25 22:09:34 -0600

    added animations

---
+++ src/main/java/com/chant/mktgPage/MktgPageApplication.java
@@ -0,0 +1,12 @@
+package com.chant.mktgPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktgPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MktgPageApplication.class, args);
+	}
+}
